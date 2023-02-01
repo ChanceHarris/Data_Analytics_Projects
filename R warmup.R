6fa418a76ba49fad2fd8b7747b0b9efbdc7f0b7c
Chance Harris <78008474+ChanceHarris@users.noreply.github.com>
2023-02-01 12:12:53 -0600

    Add files via upload

---
+++ R warmup.R
@@ -0,0 +1,26 @@
+5+6
+
+a <-5
+b <-6
+
+sum(a, b)
+
+ages <- c(5, 6)
+ages
+sum(ages)
+
+names <- C("John, James")
+names <- c("John, James")
+names
+
+Friends ,- data.frame(names, ages)
+friends <- data.frames(names, ages)
+friends <- data.frame(names, ages)
+view(friends)
+
+view(friends)
+str(friends)
+
+
+friends[1,1]
+friends[ ,1]
